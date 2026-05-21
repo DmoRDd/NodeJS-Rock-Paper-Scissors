71eb5375dee9616ee1c68f246e9ba90d31999086
Efe Aksoy <efeaydinaksoy52@gmail.com>
2026-05-21 21:45:18 +0300

    Only the design remains...

--- app.js
+++ app.js
@@ -4,60 +4,76 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
 
+  //css request
+  if (url.pathname.startsWith("/views/styles/")) {
+    res.writeHead(200, { "Content-Type": "text/css" });
+    fs.createReadStream("." + url.pathname).pipe(res);
+    return;
+  }
   switch (url.pathname) {
     case "/":
       if (req.method == "GET") {
-        const name = url.searchParams.get('name');
+        const name = url.searchParams.get('name'); //getting name from url query
         console.log(name);
+        
         res.writeHead(200, { "Content-Type": "text/html" });
-        fs.createReadStream("./index.html").pipe(res); 
+        fs.createReadStream("./views/index.html").pipe(res); 
+        
         break;
       } else if (req.method == "POST") {
-        handlePostResponse(req, res);
+        coreLoop(req, res);
         break;
       }
+    
     default:
+      
       res.writeHead(404, { "Content-Type": "text/html" });
-      fs.createReadStream("./error.html").pipe(res);
+      fs.createReadStream("./views/error.html").pipe(res);
+      
       break;
   }
 });
 
-server.listen(4001, () => {
+server.listen(3000, () => {
   console.log(`Server is listening at ${server.address().port}`);
 });
 
-function handlePostResponse(req, res) {
+
+//I could have written this inside the switch case as well :))
+function coreLoop(req, res) {
   req.setEncoding("utf8");
 
   let body = "";
-  req.on("data", function (chunk) {
+  req.on("data", chunk => {
     body += chunk;
   });
 
-  req.on("end", function () {
-    const choices = ["rock", "paper", "scissors"];
+  req.on("end", () => {
+    const choices = ["taş", "kağıt", "makas"];
     const randomChoice = choices[Math.floor(Math.random() * 3)];
 
     const choice = body;
 
     let message;
 
-    const tied = `Aww, we tied! I also chose ${randomChoice}.`;
-    const victory = `Nooooo, you won! I chose ${randomChoice}.`;
-    const defeat = `Ha! You lost. I chose ${randomChoice}.`;
+    const tied = `Ne şans ama! Benim seçimim de  ${randomChoice}tan yana oldu :).`;
+    const victory = `Ah, hayır! Sen kazandın. Ben ${randomChoice} demiştim.`;
+    const defeat = `Hahahaaa, kaybettin! Seçimim: ${randomChoice.charAt(0).toUpperCase() + randomChoice.slice(1)}.`;
 
     if (choice === randomChoice) {
+      
       message = tied;
-    } else if (
-      (choice === "rock" && randomChoice === "paper") ||
-      (choice === "paper" && randomChoice === "scissors") ||
-      (choice === "scissors" && randomChoice === "rock")
-    ) {
+
+    } else if ((choice === "taş" && randomChoice === "kağıt") || (choice === "kağıt" && randomChoice === "makas") || (choice === "makas" && randomChoice === "taş")) {
+      
       message = defeat;
+    
     } else {
+      
       message = victory;
+
     }
+
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end(`You selected ${choice}. ${message}`);
   });
